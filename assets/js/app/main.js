19cf48f4ed31130ee31b654ba26e0f6b8bb3bbff
Tehjean <tehjean_powell@hotmail.com>
2017-09-30 22:09:16 -0500

    Fifth Commit

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -1,1 +1,6 @@
-$(document).ready(function(){});
+$(document).ready(function(){
+    $("#mobile-menu-toggle").on('click', function(){
+        $("#Main-Navigation").toggleClass('dark');
+    });
+    $(".headroom").headroom({offset:768});
+});
